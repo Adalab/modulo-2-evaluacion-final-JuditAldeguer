26a31de891bd3b20b6c3048f60e54b577fb9cd5c
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-09 18:01:29 +0200

    update path for fetch

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const input = document.querySelector('.js_input');
 const searchBtn = document.querySelector('.js_search_btn');
 const resultsSection = document.querySelector('.js_results');
 const resetBtn = document.querySelector('.js_reset_btn');
+let dataMoviesAll = [];
 let dataMovies = [];
 let favourites = [];
 
@@ -12,16 +13,29 @@ getFromApi();
 function getFromApi() {
   fetch('https://api.tvmaze.com/search/shows?q=girls')
     .then((response) => response.json())
-    .then((answer) => (dataMovies = answer));
-  paintMovies();
-  setInLocalStorage();
+    .then((answer) => {
+      dataMoviesAll = answer;
+      console.log(dataMoviesAll);
+    });
+  for (const movieEl of dataMoviesAll) {
+    let newdataMovie = [
+      {
+        name: movieEl.show.name,
+        image: movieEl.show.imgage.medium,
+      },
+    ];
+    dataMovies.push(newdataMovie);
+  }
+  console.log(dataMovies);
+  //paintMovies();
+  //setInLocalStorage();
 }
 //pendiente
-function paintMovies() {} //pendiente---------
-function setInLocalStorage() {} //pendiente---------
+// function paintMovies() {} //pendiente---------
+// function setInLocalStorage() {} //pendiente---------
 function handleSearch(ev) {
   ev.preventDefault();
-  dataMovies.filter((dataEl) => dataEl.inlcudes(input.value));
+  dataMovies.filter((dataEl) => dataEl.name.toLoweCase().inlcudes(input.value));
 }
 input.addEventListener('keyup', handleSearch);
 searchBtn.addEventListener('click', handleSearch);
