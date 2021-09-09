b8d630846a1bbc1c20c19951fb733e7e0e938eff
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-09 17:27:10 +0200

    add JS structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
 'use strict';
+const searchSection = document.querySelector('.js_search');
+const input = document.querySelector('.js_input');
+const searchBtn = document.querySelector('.js_search_btn');
+const resultsSection = document.querySelector('.js_results');
+const resetBtn = document.querySelector('.js_reset_btn');
+let dataMovies = [];
+let favourites = [];
+
+getFromApi();
+
+function getFromApi() {
+  fetch('https://api.tvmaze.com/search/shows?q=girls')
+    .then((response) => response.json())
+    .then((answer) => (dataMovies = answer));
+  paintMovies();
+  setInLocalStorage();
+}
+//pendiente
+function paintMovies() {} //pendiente---------
+function setInLocalStorage() {} //pendiente---------
+function handleSearch(ev) {
+  ev.preventDefault();
+  dataMovies.filter((dataEl) => dataEl.inlcudes(input.value));
+}
+input.addEventListener('keyup', handleSearch);
+searchBtn.addEventListener('click', handleSearch);
